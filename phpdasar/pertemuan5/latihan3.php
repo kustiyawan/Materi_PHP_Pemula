af1566a472094b4872fe7970c5c8b39a6aa577d3
Hadi Kustiyawan <80027604+kustiyawan@users.noreply.github.com>
2024-01-24 09:34:31 +0700

    Update latihan3.php

--- phpdasar/pertemuan5/latihan3.php
+++ phpdasar/pertemuan5/latihan3.php
@@ -11,6 +11,17 @@ $mahasiswa = [
     ["Baim Wong", "043364190", "Teknik Informatika", "baimwong@yahoo.com"],
     ["Erik", "023040123", "Teknik Planologi", "erik@gmail.com"]
 
+    // sedikit informasi, jikalau memakai teknik ini urutan yang diatas tidak boleh salah, harus sesuai tampilan
+    // daftar mana sesuai yang kita pengen, seperti contoh di bawah mencoba untuk membuat daftar dengan urutan 
+    // nama -> nrp -> jurusan -> email
+    // data array harus diurutkan sesuai contoh urutan di atas, maka array yang di pakai untuk teknik ini disebut array numerik
+    // array yang dimana index nya angka
+    // dipenjelasan sebelumnya array adalah pasangan antara key dan value, key nya adalah index yang dimulai dari 0,
+    // jadi ada cara biar fleksibel yaitu mengubah array numerik dengan array associative
+    // yang dimana index nya bukan lagi angka tapi index nya adalah string
+    // yang kita buat sendiri untuk mengasosiasikan ke nilai yang ada di dalam array nya 
+    // index nya berasosiasi dengan nilainya 
+
 ];
 
 ?>
