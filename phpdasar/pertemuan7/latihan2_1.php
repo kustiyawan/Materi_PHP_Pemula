bbfd2d7acccb50fa0175ca8134964c3cc043aacb
Hadi Kustiyawan <80027604+kustiyawan@users.noreply.github.com>
2024-06-18 08:08:48 +0700

    Update latihan2_1.php
    Membuat perkondisian

--- phpdasar/pertemuan7/latihan2_1.php
+++ phpdasar/pertemuan7/latihan2_1.php
@@ -1,3 +1,16 @@
+<!-- semisal ada yang iseng langsung ke url file latihan2_1.php tanpa kudu klik link yang di file latihan1_4.php
+    sistem akan mengalamai error karena gak ada data yang dikirimkan dari klik kan link yang di file latihan1_4.php
+    link url latihan2_1.php akan berusaha mencetak sesuatu yang gak ada -->
+
+    <!-- untuk cara mengatasinya, gimana supaya user tidak langsung masuk ke latihan2_1.php lewat ngetik di link url 
+        tapi harus lewat link yang ada di latihan1_4.php yang memang kudu di klik -->
+
+<!-- dibawah sini akan di buat per kondisian -->
+<?php 
+    // cek apakah tidak ada data di $_GET
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
