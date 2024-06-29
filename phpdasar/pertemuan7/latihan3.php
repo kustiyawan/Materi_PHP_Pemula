e93a55e2d4d6a0d21965082d33bb93935e9ae0ad
Hadi Kustiyawan <80027604+kustiyawan@users.noreply.github.com>
2024-06-29 11:46:18 +0700

    Add files via upload

---
+++ phpdasar/pertemuan7/latihan3.php
@@ -0,0 +1,7 @@
+<!-- selanjutnya adalah Request Method Post 
+    sebelumnya adalah penggunaan get yang memang menggunakan URL di POST akan menggunakan konsep FORM 
+    di GET yang memakai URL lebih rentan, karena data bisa terlihat di link
+    karena itu lah di POST ini penggunaa form akan lebih fleksibel, karena bisa digunakan method POST dan GET
+    ketika menggunakan form data yang dikirimkan tidak akan kelihatan
+    semisal contoh pembuatan akun pasti ada username password, jika memakai konsep GET dan link URL, user dan password akan terlihat di link
+    dan ini yang bahaya, maka dari itu penggunaan form lebih di anjurkan di data data penting -->
